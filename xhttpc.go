d46250771b9de917dfcb64f32d468a3247c5645e
Yuichi Watanabe <yuichi.watanabe.ja@gmail.com>
2019-08-04 18:38:45 +0900

    change api interface

--- xhttpc.go
+++ xhttpc.go
@@ -2,6 +2,7 @@ package xhttpc
 
 import (
 	"context"
+	"fmt"
 	"io"
 	"net/http"
 	"net/url"
@@ -15,11 +16,11 @@ import (
 	"os"
 )
 
-type Response struct {
+type XResponse struct {
 	*http.Response
 }
 
-func (resp *Response) DecodeJson(v interface{}) error {
+func (resp *XResponse) DecodeJson(v interface{}) error {
 	var reader io.ReadCloser
 	var err error
 	switch resp.Header.Get("Content-Encoding") {
@@ -31,7 +32,9 @@ func (resp *Response) DecodeJson(v interface{}) error {
 	default:
 		reader = resp.Body
 	}
-	defer reader.Close()
+	defer func(r io.ReadCloser) {
+		_ = r.Close()
+	}(reader)
 
 	decErr := json.NewDecoder(reader).Decode(v)
 	if decErr == io.EOF {
@@ -44,12 +47,12 @@ func (resp *Response) DecodeJson(v interface{}) error {
 	return nil
 }
 
-func (resp *Response) Copy(w io.Writer) (written int64, err error) {
+func (resp *XResponse) Copy(w io.Writer) (written int64, err error) {
 	written, err = io.Copy(w, resp.Body)
 	return
 }
 
-func (resp *Response) ReadAll() ([]byte, error) {
+func (resp *XResponse) ReadAll() ([]byte, error) {
 	var reader io.ReadCloser
 	var err error
 	switch resp.Header.Get("Content-Encoding") {
@@ -61,11 +64,13 @@ func (resp *Response) ReadAll() ([]byte, error) {
 	default:
 		reader = resp.Body
 	}
-	defer reader.Close()
+	defer func(r io.ReadCloser) {
+		_ = r.Close()
+	}(reader)
 	return ioutil.ReadAll(reader)
 }
 
-func (resp *Response) String() (string, error) {
+func (resp *XResponse) String() (string, error) {
 	v, err := resp.ReadAll()
 	if err != nil {
 		return "", err
@@ -79,87 +84,114 @@ func (h Header) Set(k, v string) {
 	h[k] = v
 }
 
-func NewClient(client *http.Client) *Client {
-	return &Client{
-		client:    client,
-		Header:    make(map[string]string),
-		BaseQuery: url.Values{},
+func NewXClient(client *http.Client) *XClient {
+	if client == nil {
+		client = http.DefaultClient
+	}
+	return &XClient{
+		Client:     client,
+		BaseHeader: make(map[string]string),
+		BaseQuery:  url.Values{},
 	}
 }
 
-type Client struct {
-	client    *http.Client
-	Header    Header
-	BaseQuery url.Values
+type XClient struct {
+	*http.Client
+	BaseHeader Header
+	BaseQuery  url.Values
 }
 
-func (c *Client) Post(ctx context.Context, u *url.URL, body url.Values) (*Response, error) {
-	return c.call(ctx, http.MethodPost, c.resolveURL(u), body)
-}
-
-func (c *Client) Put(ctx context.Context, u *url.URL, body url.Values) (*Response, error) {
-	return c.call(ctx, http.MethodPut, c.resolveURL(u), body)
-}
-
-func (c *Client) Delete(ctx context.Context, u *url.URL, query url.Values) (res *Response, err error) {
-	return c.call(ctx, http.MethodDelete, c.resolveURL(u, query), nil)
-}
-
-func (c *Client) Get(ctx context.Context, u *url.URL, query url.Values) (res *Response, err error) {
-	return c.call(ctx, http.MethodGet, c.resolveURL(u, query), nil)
-}
-
-func (c *Client) call(ctx context.Context, method, url string, body url.Values) (*Response, error) {
-	req, err := c.newRequest(method, url, body)
+func (c *XClient) Post(ctx context.Context, u *url.URL, body interface{}, header Header) (*XResponse, error) {
+	values, err := toValues(body)
 	if err != nil {
 		return nil, err
 	}
-	resp, err := c.do(ctx, req)
+	return c.call(ctx, http.MethodPost, c.resolveURL(u), values, header)
+}
+
+func (c *XClient) Put(ctx context.Context, u *url.URL, body interface{}, header Header) (*XResponse, error) {
+	values, err := toValues(body)
+	if err != nil {
+		return nil, err
+	}
+	return c.call(ctx, http.MethodPut, c.resolveURL(u), values, header)
+}
+
+func (c *XClient) Delete(ctx context.Context, u *url.URL, query interface{}, header Header) (res *XResponse, err error) {
+	values, err := toValues(query)
+	if err != nil {
+		return nil, err
+	}
+	return c.call(ctx, http.MethodDelete, c.resolveURL(u, values), nil, header)
+}
+
+func (c *XClient) Get(ctx context.Context, u *url.URL, query interface{}, header Header) (res *XResponse, err error) {
+	values, err := toValues(query)
+	if err != nil {
+		return nil, err
+	}
+	return c.call(ctx, http.MethodGet, c.resolveURL(u, values), nil, header)
+}
+
+func (c *XClient) call(ctx context.Context, method, url string, body url.Values, header Header) (*XResponse, error) {
+	req, err := c.NewRequest(method, url, body, header)
+	if err != nil {
+		return nil, err
+	}
+	resp, err := c.XDo(ctx, req)
 	if err != nil {
 		return resp, err
 	}
 	return resp, nil
 }
 
-func (c *Client) newRequest(method, url string, body url.Values) (*http.Request, error) {
+func (c *XClient) NewRequest(method, url string, body url.Values, header Header) (*http.Request, error) {
 	var buf io.Reader
 	if body != nil {
 		buf = strings.NewReader(body.Encode())
 	}
-
 	req, err := http.NewRequest(method, url, buf)
 	if err != nil {
 		return nil, err
 	}
-
-	for k, v := range c.Header {
+	for k, v := range c.BaseHeader {
 		req.Header.Set(k, v)
+	}
+	if header != nil {
+		for k, v := range header {
+			req.Header.Set(k, v)
+		}
 	}
 	return req, nil
 }
 
-func (c *Client) newUploadRequest(url string, reader io.Reader, size int64, mediaType string) (*http.Request, error) {
+func (c *XClient) NewUploadRequest(url string, reader io.Reader, size int64, mediaType string, header Header) (*http.Request, error) {
 	req, err := http.NewRequest(http.MethodPost, url, reader)
 	if err != nil {
 		return nil, err
 	}
 	req.ContentLength = size
 
-	for k, v := range c.Header {
+	for k, v := range c.BaseHeader {
 		req.Header.Set(k, v)
 	}
 	req.Header.Set("Content-Type", mediaType)
+	if header != nil {
+		for k, v := range header {
+			req.Header.Set(k, v)
+		}
+	}
 	return req, nil
 }
 
-func (c *Client) NewMultipartRequest(url string, values map[string]io.Reader) (*http.Request, error) {
+func (c *XClient) NewMultipartRequest(url string, values map[string]io.Reader, header Header) (*http.Request, error) {
 	var buffer bytes.Buffer
 	multipartWriter := multipart.NewWriter(&buffer)
 	for key, reader := range values {
 		var fieldWriter io.Writer
 		var err error = nil
 		if closable, ok := reader.(io.Closer); ok {
-			closable.Close()
+			_ = closable.Close()
 		}
 		if file, ok := reader.(*os.File); ok {
 			if fieldWriter, err = multipartWriter.CreateFormFile(key, file.Name()); err != nil {
@@ -174,22 +206,26 @@ func (c *Client) NewMultipartRequest(url string, values map[string]io.Reader) (*
 			return nil, err
 		}
 	}
-	multipartWriter.Close()
+	_ = multipartWriter.Close()
 	req, err := http.NewRequest(http.MethodPost, url, &buffer)
 	if err != nil {
 		return nil, err
 	}
-	for k, v := range c.Header {
+	for k, v := range c.BaseHeader {
 		req.Header.Set(k, v)
 	}
 	req.Header.Set("Content-Type", multipartWriter.FormDataContentType())
+	if header != nil {
+		for k, v := range header {
+			req.Header.Set(k, v)
+		}
+	}
 	return req, nil
 }
 
-func (c *Client) do(ctx context.Context, req *http.Request) (*Response, error) {
+func (c *XClient) XDo(ctx context.Context, req *http.Request) (*XResponse, error) {
 	req = req.WithContext(ctx)
-
-	resp, err := c.client.Do(req)
+	resp, err := c.Client.Do(req)
 	if err != nil {
 		select {
 		case <-ctx.Done():
@@ -199,10 +235,10 @@ func (c *Client) do(ctx context.Context, req *http.Request) (*Response, error) {
 		return nil, err
 	}
 
-	return &Response{Response: resp}, nil
+	return &XResponse{Response: resp}, nil
 }
 
-func (c *Client) resolveURL(u *url.URL, queries ...url.Values) string {
+func (c *XClient) resolveURL(u *url.URL, queries ...url.Values) string {
 	q := url.Values{}
 	for _, query := range queries {
 		keys := make([]string, 0, len(query))
@@ -216,15 +252,48 @@ func (c *Client) resolveURL(u *url.URL, queries ...url.Values) string {
 	}
 	u.RawQuery = q.Encode()
 	baseQuery := c.BaseQuery.Encode()
-
-	// TODO
-	if baseQuery != "" {
-		if u.RawQuery == "" {
-			return u.String() + "?" + baseQuery
-		} else {
-			return u.String() + "&" + baseQuery
-		}
+	rawURL := u.String()
+	if baseQuery == "" {
+		return rawURL
 	}
+	if u.RawQuery == "" {
+		return rawURL + "?" + baseQuery
+	}
+	return rawURL + "&" + baseQuery
+}
 
-	return u.String()
+func toValues(data interface{}) (url.Values, error) {
+	if data == nil {
+		return url.Values{}, nil
+	}
+	result := make(map[string]interface{})
+	b, err := json.Marshal(data)
+	if err != nil {
+		return nil, err
+	}
+	d := json.NewDecoder(strings.NewReader(string(b)))
+	d.UseNumber()
+	if err := d.Decode(&result); err != nil {
+		return nil, err
+	}
+	values := url.Values{}
+	for k, v := range result {
+		addValues(values, k, v)
+	}
+	return values, nil
+}
+
+func addValues(values url.Values, k string, v interface{}) {
+	switch as := v.(type) {
+	case []interface{}:
+		for _, v := range as {
+			addValues(values, k, v)
+		}
+	case map[string]interface{}:
+		for mapKey, v := range as {
+			addValues(values, fmt.Sprintf(k, mapKey), v)
+		}
+	default:
+		values.Add(k, fmt.Sprintf("%v", v))
+	}
 }
